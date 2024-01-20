302c20bc0a15cd245dc0b0e1ad1fb3444b63d857
Gouransh <147132368+Gouranshvaishnavji@users.noreply.github.com>
2024-01-20 12:30:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,95 +16,104 @@ veeru.addEventListener("click",function(){
     }
 });
 
-// let sachinimg=document.getElementById("m-blaster-img")
-// let sachin=document.getElementById("m-blaster")
-// sachin.addEventListener("click",function(){
-//     sachinimg.style.display="block";
-//     sachinimg.onmouseleave=()=>{
-//         sachinimg.style.display="none";
-//     }
-// })
+let sachinimg=document.getElementById("m-blaster-img")
+let sachin=document.getElementById("m-blaster")
+sachin.addEventListener("click",function(){
+    event.preventDefault();
+    sachinimg.style.display="block";
+    sachinimg.onmouseleave=()=>{
+        sachinimg.style.display="none";
+    }
+})
 
-// let gautamimg=document.getElementById("bjp-img")
-// let gautam=document.getElementById("bjp")
-// gautam.addEventListener("click",function(){
-//     gautamimg.style.display="block";
-//     gautamimg.onmouseleave=()=>{
-//         gautamimg.style.display="none";
-//     }
-// })
-// let viratimg=document.getElementById("chiku-img")
-// let virat=document.getElementById("chiku")
-// virat.addEventListener("click",function(){
-//     viratimg.style.display="block";
-//     viratimg.onmouseleave=()=>{
-//         viratimg.style.display="none";
-//     }
-// })
+let gautamimg=document.getElementById("bjp-img")
+let gautam=document.getElementById("bjp")
+gautam.addEventListener("click",function(){
+    event.preventDefault();
+    gautamimg.style.display="block";
+    gautamimg.onmouseleave=()=>{
+        gautamimg.style.display="none";
+    }
+})
+let viratimg=document.getElementById("chiku-img")
+let virat=document.getElementById("chiku")
+virat.addEventListener("click",function(){
+    event.preventDefault();
+    viratimg.style.display="block";
+    viratimg.onmouseleave=()=>{
+        viratimg.style.display="none";
+    }
+})
 
-// let dhoniImg=document.getElementById("mahi-img")
-// let dhoni=document.getElementById("mahi")
-// dhoni.addEventListener("click",function(){
-//     dhoniImg.style.display="block";
-//     dhoniImg.onmouseleave=()=>{
-//         dhoniImg.style.display="none";
-//     }
-// })
+let dhoniImg=document.getElementById("mahi-img")
+let dhoni=document.getElementById("mahi")
+dhoni.addEventListener("click",function(){
+    event.preventDefault();
+    dhoniImg.style.display="block";
+    dhoniImg.onmouseleave=()=>{
+        dhoniImg.style.display="none";
+    }
+})
 
-// let yuvrajimg=document.getElementById("yuvi-img")
-// let yuvraj=document.getElementById("yuvi")
-// yuvraj.addEventListener("click",function(){
-//     yuvrajimg.style.display="block";
-//     yuvrajimg.onMouseLeave=()=>{
-//         yuvrajimg.style.display="none";
-//     }
-// })
+let yuvrajimg=document.getElementById("yuvi-img")
+let yuvraj=document.getElementById("yuvi")
+yuvraj.addEventListener("click",function(){
+    event.preventDefault();
+    yuvrajimg.style.display="block";
+    yuvrajimg.onMouseLeave=()=>{
+        yuvrajimg.style.display="none";
+    }
+})
 
-// let sureshimg=document.getElementById("rane-img")
-// let suresh=document.getElementById("rane")
-// suresh.addEventListener("click",function(){
-//     sureshimg.style.display="block";
+let sureshimg=document.getElementById("rane-img")
+let suresh=document.getElementById("rane")
+suresh.addEventListener("click",function(){
+    event.preventDefault();
+    sureshimg.style.display="block";
     
-//     sureshimg.onMouseLeave=()=>{
-//         sureshimg.style.display="none";
-//     }
-// })
+    sureshimg.onMouseLeave=()=>{
+        sureshimg.style.display="none";
+    }
+})
 
 
-// let singhimg=document.getElementById("bhajji-img")
-// let singh=document.getElementById("singh")
-// singh.addEventListener("click",function(){
-//     singhimg.style.display="block";
+let singhimg=document.getElementById("bhajji-img")
+let singh=document.getElementById("singh")
+singh.addEventListener("click",function(){
+    event.preventDefault();
+    singhimg.style.display="block";
     
-//     singhimg.onMouseLeave=()=>{
-//         singhimg.style.display="none";
-//     }
-// })
+    singhimg.onMouseLeave=()=>{
+        singhimg.style.display="none";
+    }
+})
 
-// let khanimg=document.getElementById("khan-img")
-// let khan=document.getElementById("khan")
-// khan.addEventListener("click",function(){
-//     khanimg.style.display="block";
-//     khanimg.onmouseleave=()=>{
-//         khanimg.style.display="none";
-//     }
-// })
+let khanimg=document.getElementById("khan-img")
+let khan=document.getElementById("khan")
+khan.addEventListener("click",function(){
+    event.preventDefault();
+    khanimg.style.display="block";
+    khanimg.onmouseleave=()=>{
+        khanimg.style.display="none";
+    }
+})
 
 
-// let patelimg=document.getElementById("patel-img")
-// let patel=document.getElementById("patel")
-// patel.addEventListener("click",function(){
-//     patelimg.style.display="block";
-//     patelimg.onmouseleave=()=>{
-//         patelimg.style.display="none";
-//     }
-// })
+let patelimg=document.getElementById("patel-img")
+let patel=document.getElementById("patel")
+patel.addEventListener("click",function(){
+    event.preventDefault();
+    patelimg.style.display="block";
+    patelimg.onmouseleave=()=>{
+        patelimg.style.display="none";
+    }
+})
 
-// let sreeshantimg=document.getElementById("shanth-img")
-// let sreeshant=document.getElementById("shanth")
-// sreeshant.addEventListener("click",function(){
-//     sreeshantimg.style.display="block";
-//     sreeshantimg.onmouseleave=()=>{
-//         sreeshantimg.style.display="none";
-//     }
-// })
+let sreeshantimg=document.getElementById("shanth-img")
+let sreeshant=document.getElementById("shanth")
+sreeshant.addEventListener("click",function(){
+    sreeshantimg.style.display="block";
+    sreeshantimg.onmouseleave=()=>{
+        sreeshantimg.style.display="none";
+    }
+})
